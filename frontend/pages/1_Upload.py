3ae0dba96c853104e7441a3284c27fc90724e770
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-04-19 13:58:57 +0530

    Update 1_Upload.py

--- frontend/pages/1_Upload.py
+++ frontend/pages/1_Upload.py
@@ -3,7 +3,7 @@ from utils import upload_csv
 
 st.title(" Upload Student Data")
 
-file = st.file_uploader("Upload CSV", type=["csv","xls"], accept_multiple_files =True)
+file = st.file_uploader("Upload CSV", type=["csv","xls"], accept_multiple_files =False)
 # st.file_uploader(label, type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, max_upload_size=None, disabled=False, label_visibility="visible", width="stretch")
 if file:
     st.info("File ready") ## st.info / st.write -- print
